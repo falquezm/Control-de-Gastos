48eb1470f86914c5d0f21e82219694bdf462a0a6
falquezm <90979848+falquezm@users.noreply.github.com>
2024-09-22 15:47:36 -0500

    Segundo proyecto #InmersionDev #AluraLatam
    Proyecto Control de gastos segunda clase inmersión dev Alura

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+let listaNombresGastos = [];
+let listaValoresGastos = [];
+
+function agregarGasto() {
+    let nombreGasto = document.getElementById('nombreGasto').value;
+    let valorGasto = document.getElementById('valorGasto').value;
+
+    listaNombresGastos.push(nombreGasto);
+    listaValoresGastos.push(valorGasto);
+}
+
+function actualizarListaGastos() {
+    const listaElementos = document.getElementById('listaGastos');
+    const totalElementos = document.getElementById('totalGastos');
+    let htmlLista = '';
+    let valorGasto = 0;
+    listaNombresGastos.forEach((element, position) => {
+       const listaValoresGastos = Number(listaValoresGastos[position]).toFixed(2);
+       htmlLista += `<li>${element} - USD ${valorGasto}
+                    <button onclick="eliminarGasto(${position});">Eliminar</button>
+                    </li>`;
+       totalGastos += Number(valorGasto);
+    });
+    listaElementos.innerHTML = htmlLista;
+    totalElementos.innerHTML = totalGastos.toFixed(2);
+    limpiar();
+}
+
+function limpiar() {
+    document.getElementById('nombreGasto').value = '';
+    document.getElementById('valorGasto').value = '';
+}
+
+function eliminarGasto(position) {
+    listaNombresGastos.splice(position,1);
+    listaValoresGastos.splice(position,1);
+    actualizarListaGastos();
+}
