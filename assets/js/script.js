31d8047feb3ac524bf4791030fb22ea3fd5c64ce
falquezm <90979848+falquezm@users.noreply.github.com>
2024-09-22 17:27:27 -0500

    Add files via upload
    Actualización proyecto con desafíos:  Agregar descripción del gatos y funcionalidad de modificar.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,62 @@
-let listaNombresGastos = [];
-let listaValoresGastos = [];
+let listaGastos = [];
 
 function agregarGasto() {
+    let fechaGasto = document.getElementById('fechaGasto').value;
     let nombreGasto = document.getElementById('nombreGasto').value;
+    let descripcionGasto = document.getElementById('descripcionGasto').value;
     let valorGasto = document.getElementById('valorGasto').value;
 
-    listaNombresGastos.push(nombreGasto);
-    listaValoresGastos.push(valorGasto);
+    listaGastos.push([fechaGasto,nombreGasto,descripcionGasto,Number(valorGasto).toFixed(2)]);
+    actualizarListaGastos();
 }
 
 function actualizarListaGastos() {
-    const listaElementos = document.getElementById('listaGastos');
+    const listaElementos = document.getElementById('listaDeGastos');
     const totalElementos = document.getElementById('totalGastos');
     let htmlLista = '';
-    let valorGasto = 0;
-    listaNombresGastos.forEach((element, position) => {
-       const listaValoresGastos = Number(listaValoresGastos[position]).toFixed(2);
-       htmlLista += `<li>${element} - USD ${valorGasto}
-                    <button onclick="eliminarGasto(${position});">Eliminar</button>
-                    </li>`;
-       totalGastos += Number(valorGasto);
+    let totalGastos = 0;
+    listaGastos.forEach((element, position) => {
+        htmlLista += `<li>${element[0]} -  ${element[1]} -  ${element[2]} - USD $${element[3]}
+                        <button onclick="modificarGasto(${position});">Modificar</button>
+                        <button onclick="eliminarGasto(${position});">Eliminar</button>
+                     </li>`;
+        totalGastos += Number(element[3]);
     });
+
     listaElementos.innerHTML = htmlLista;
     totalElementos.innerHTML = totalGastos.toFixed(2);
     limpiar();
 }
 
 function limpiar() {
+    document.getElementById('fechaGasto').value = '';
     document.getElementById('nombreGasto').value = '';
+    document.getElementById('descripcionGasto').value = '';
     document.getElementById('valorGasto').value = '';
+    document.getElementById('botonFormulario').textContent = "Agregar Datos";
+    document.getElementById('botonFormulario').onclick = function() { agregarGasto(); };
 }
 
 function eliminarGasto(position) {
-    listaNombresGastos.splice(position,1);
-    listaValoresGastos.splice(position,1);
+    listaGastos.splice(position,1);
+    actualizarListaGastos();
+}
+
+function modificarGasto(position) {
+    document.getElementById('fechaGasto').value = listaGastos[position][0];
+    document.getElementById('nombreGasto').value = listaGastos[position][1];
+    document.getElementById('descripcionGasto').value = listaGastos[position][2];
+    document.getElementById('valorGasto').value = listaGastos[position][3];
+    document.getElementById('botonFormulario').textContent = "Actualizar Datos";
+    document.getElementById('botonFormulario').onclick = function() { actualizarGasto(position); };
+}
+
+function actualizarGasto(position) {
+    let fechaGasto = document.getElementById('fechaGasto').value;
+    let nombreGasto = document.getElementById('nombreGasto').value;
+    let descripcionGasto = document.getElementById('descripcionGasto').value;
+    let valorGasto = document.getElementById('valorGasto').value;
+    
+    listaGastos[position] = [fechaGasto,nombreGasto,descripcionGasto,Number(valorGasto).toFixed(2)];
     actualizarListaGastos();
 }
